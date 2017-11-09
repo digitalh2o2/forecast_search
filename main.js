d5cf5d50644797d04ddfab78f5b3457b32c220df
Saul <digitalh2o@gmail.com>
2017-11-08 21:56:52 -0600

    Created list items from user search and added Event Listeners. Next updates will pulling data from click events

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 console.log("hi from console")
+// original search query by user
 const userSearch = "http://autocomplete.wunderground.com/aq?query=";
+
+// various document selectors
 const userInput = document.querySelector('.input');
+//const cityName = document.querySelector('datalist');
 let cityOptions = document.querySelector('#cities');
 
 
@@ -10,28 +14,47 @@ userInput.addEventListener('keyup',function(e){
   }
 })
 
+cityOptions.addEventListener('click',function(e){
+  userInput.value = e.target.textContent;
+  console.log(e.target.attributes["0"].value);
+  cityOptions.innerHTML = ""
+})
+
 // Search Function As User Types
 function getLocation(){
+  // Get current value within the input field
   const locationSearch = userInput.value;
+  // Fetch the location Promise through the API
+  const locationPromise = fetch(userSearch + locationSearch);
 
-  const location = fetch(userSearch + locationSearch);
-
-  location
+  locationPromise
+  .then(function(response){
+    // retrieve the response and turn to JSON
+    response.json()
     .then(function(response){
-      response.json()
-      .then(function(response){
-        const data = response.RESULTS;
-        cityOptions.innerHTML = "";
+      // store the response in data variable
+      const data = response.RESULTS;
 
-        data.forEach(function(item){
-          console.log(item.name)
-          let option = document.createElement('option');
-          option.value = item.name;
-          cityOptions.appendChild(option);
-        })
+      // clear out the unordered list
+      cityOptions.innerHTML = "";
+
+      // iterate through each result in the data
+      data.forEach(function(item){
+        console.log(item)
+        // create new elements that will store the information
+        let link = document.createElement('a');
+        let details = document.createElement('li');
+        // set attribute and name of individual list item in order to get final information
+        details.setAttribute("zmw", item.zmw);
+        details.innerHTML = item.name;
+        // append the details to the anchor and then finally to the unordered list
+        link.appendChild(details);
+        cityOptions.appendChild(link);
       })
-      .catch(function(err){
-        console.log(err)
-      })
-    });
+    })
+    // otherwise toss error if there is one
+    .catch(function(err){
+      console.log(err)
+    })
+  });
 }
