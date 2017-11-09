e72f4e3b3f9efc3d5f40d579d80799a4639590db
Saul <digitalh2o@gmail.com>
2017-11-08 19:13:46 -0600

    Options populate to search input as user types

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 console.log("hi from console")
 const userSearch = "http://autocomplete.wunderground.com/aq?query=";
 const userInput = document.querySelector('.input');
+let cityOptions = document.querySelector('#cities');
+
 
 userInput.addEventListener('keyup',function(e){
-  getLocation();
+  if(userInput.value.length >= 3){
+    getLocation();
+  }
 })
 
-// Search Function
+// Search Function As User Types
 function getLocation(){
   const locationSearch = userInput.value;
 
@@ -16,7 +20,15 @@ function getLocation(){
     .then(function(response){
       response.json()
       .then(function(response){
-        console.log(response)
+        const data = response.RESULTS;
+        cityOptions.innerHTML = "";
+
+        data.forEach(function(item){
+          console.log(item.name)
+          let option = document.createElement('option');
+          option.value = item.name;
+          cityOptions.appendChild(option);
+        })
       })
       .catch(function(err){
         console.log(err)
